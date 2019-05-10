00f21930a40595949f6ffe844caa43849fe68ff0
Eya-AFFES <37873048+Eya-AFFES@users.noreply.github.com>
2019-05-10 16:15:00 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,17 +20,24 @@ __author__ = 'Eya-AFFES'
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
+#Importing the required libraries. These libraries will be required on every Skill
 
 import serial
 ser00 = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 
 LOGGER = getLogger(__name__)
+#This section starts logging of the Skill in the mycroft-skills.log file. 
+#If you remove this line, your Skill will not log any errors, and you will have difficulty debugging.
 
 class MoveStopSkill(MycroftSkill):
     def __init__(self):
+        #This method is the constructor, and the key function it has is to define the name of the Skill.
         super(MoveStopSkill, self).__init__(name="MoveStopSkill")
-
+        
     def initialize(self):
+        #initialize()function defines each of the Intents of the Skill. Note that there are three Intents defined in
+        
+        #three Intents defined in vocab files
         thank_you_intent = IntentBuilder("ThankYouIntent").require("ThankYouKeyword").build()
         self.register_intent(thank_you_intent, self.handle_thank_you_intent)
 
@@ -40,22 +47,36 @@ class MoveStopSkill(MycroftSkill):
         hello_world_intent = IntentBuilder("HelloWorldIntent").require("HelloWorldKeyword").build()
         self.register_intent(hello_world_intent ,self.handle_hello_world_intent)
 
+    def handle_hello_world_intent(self, message):
+        # A method that handles the Intent hello_world_intent
+        #2 parameters
+        #self is the reference to the object itself
+        #message is an incoming message from the messagebus
+        self.speak_dialog("hello.world")
+        #hello.world dialog is passed to the  speak_dialog() method
+        #this is defined in the file “hello.world.dialog”
+        msg="MVF"
+        ser00.write(bytes(msg, 'utf-8')) 
+
+    def handle_how_are_you_intent(self, message):
+        # A method that handles the Intent how_are_you_intent
+        self.speak_dialog("how.are.you")
+        msg="MVB"
+        ser00.write(bytes(msg, 'utf-8'))
+        
     def handle_thank_you_intent(self, message):
+        # A method that handles the Intent thank_you_intent
         self.speak_dialog("welcome")
         msg="ST"
         ser00.write(bytes(msg, 'utf-8')) 
 
-    def handle_how_are_you_intent(self, message):
-        self.speak_dialog("how.are.you")
-        msg="MVB"
-        ser00.write(bytes(msg, 'utf-8')) 
-
-    def handle_hello_world_intent(self, message):
-        self.speak_dialog("hello.world")
-        msg="MVF"
-        ser00.write(bytes(msg, 'utf-8')) 
+    
 
     def stop(self):
+        #This method tells Mycroft what to do if a stop intent is detected.
+        #the pass statement is used as a placeholder; it doesn’t actually have any function. 
+        #However, if the Skill had any active functionality, the stop() method would terminate 
+        #the functionality, leaving the *Skill** in a known good state.
         pass
 
 
